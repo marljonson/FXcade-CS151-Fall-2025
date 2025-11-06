4c7eeaac3ec314aab1e6739edfb02497869e6f18
Marl Jonson <marl.jonson@comcast.net>
2025-11-06 10:35:31 -0800

    Actually created file structure with starter classes or placeholder .txt files

---
+++ src/test/java/manager/GameManagerTest.java
@@ -0,0 +1,9 @@
+package test.java.manager;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameManagerTest {
+    
+}
