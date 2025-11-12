3cd49b5ee3d84ab464bd920a3cad6e4d35bb5422
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-11-11 19:23:46 -0800

    Hand: get toals by accounting for Aces

--- src/main/java/blackjack/Hand.java
+++ src/main/java/blackjack/Hand.java
@@ -10,5 +10,30 @@ public class Hand {
     public void add(Card card){ cards.add(Objects.requireNonNull(card)); }      // add card to hand
     public int size(){ return cards.size(); }                                   // Number of cards in hand
 
-    
+    /*
+     * Calculate total for hand, Ace may be 1 or 11
+     * Unique total
+     */
+    public List<Integer> getTotals(){
+        int sum = 0;
+        int aceCount = 0;
+
+        for(Card card : cards){
+            sum += card.getValue();
+            if (card.getValue() == 11) {
+                aceCount++;
+            }
+        }
+
+        // Adjust for Aces, also preserve insertion order for sorting
+        Set<Integer> totals = new LinkedHashSet<>();
+        int current = sum;
+        for(int i = 0; i <= aceCount; i++){
+            totals.add(current);
+            current -= 10; // reduce ace from 11 to 1 prevent busting
+        }
+        List<Integer> sorted = new ArrayList<>(totals);
+        Collections.sort(sorted);
+        return sorted;
+    }
 }
