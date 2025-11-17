58dacae47fcd905a7d8729424f93f8768fc7313e
Bush Nguyen <bush.nguyen02@gmail.com>
2025-11-17 07:58:34 -0800

    create each block segment of the snake

---
+++ FXcade-CS151-Fall-2025/src/snake/model/SnakeSegment.java
@@ -0,0 +1,17 @@
+package snake.model;
+
+public class SnakeSegment {
+    private int x;
+    private int y;
+
+    public SnakeSegment(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+}
