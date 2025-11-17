8e51bddf50fe253f993cd47ae1e825887ba9a57b
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-11-17 11:48:32 -0800

    Participant helper class added for game logic

---
+++ src/main/java/blackjack/BlackjackGame.java
@@ -0,0 +1,10 @@
+package main.java.blackjack;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BlackjackGame {
+    // related to soft total added to 17 when ace is 11
+    private static final boolean DEALER_HITS_SOFT_17 = true;
+    
+}
---
+++ src/main/java/blackjack/Participant.java
@@ -0,0 +1,39 @@
+package main.java.blackjack;
+
+public class Participant {
+    private final String name;
+    private final boolean human;
+    private final boolean dealer;
+    private final Hand hand = new Hand();
+
+    private int bankroll = 1000; // default starting money
+    private int bet = 0;
+
+    public Participant(String name, boolean human, boolean dealer) {
+        this.name = name;
+        this.human = human;
+        this.dealer = dealer;
+    }
+
+    // player related getters
+    public String getName() { return name; }
+    public boolean isHuman() { return human; }
+    public boolean isDealer() { return dealer; }
+    public Hand getHand() { return hand; }
+
+    // game related methods
+    public int getBankroll() { return bankroll; }
+    public int getBet() { return bet; }
+    public void setBet(int amount) { this.bet = amount; }
+    public void win(int amount){ bankroll += amount; }
+    public void lose(int amount){ bankroll -= amount; }
+
+    public void clearForNextRound(){
+        hand.clear();
+        bet = 0;
+    }
+    
+
+    
+    
+}
