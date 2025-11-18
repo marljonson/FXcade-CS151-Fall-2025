d0b77ac10a1a031ecef9ae71fa5d3053ab16fd10
Bush Nguyen <bush.nguyen02@gmail.com>
2025-11-17 17:43:57 -0800

    postion of food

---
+++ FXcade-CS151-Fall-2025/src/snake/model/Food.java
@@ -0,0 +1,14 @@
+package snake.model;
+
+public class Food {
+    private int x;
+    private int y;
+
+    public Food(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+}
