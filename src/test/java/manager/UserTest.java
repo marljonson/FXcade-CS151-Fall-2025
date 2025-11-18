2cf9b1acafadb68a653b1eee04ef8a9224c1bdf1
Marl Jonson <marl.jonson@comcast.net>
2025-11-18 11:19:27 -0800

    Create function testSetters() in UserTest

--- src/test/java/manager/UserTest.java
+++ src/test/java/manager/UserTest.java
@@ -10,6 +10,25 @@ class UserTest {
         User user = new User("alice", "password123");
         assertEquals("alice", user.getUsername());
         assertEquals("password123", user.getPassword());
-        assertEquals(0, user.getHighScore()); // default highScore
+        assertEquals(0, user.getHighScore()); // 0 is the default highScore
+    }
+
+    @Test
+    void testConstructorWithHighScore() {
+        User user = new User("cheshire", "password123", 100);
+        assertEquals("cheshire", user.getUsername());
+        assertEquals("password123", user.getPassword());
+        assertEquals(100, user.getHighScore());
+    }
+
+    @Test
+    void testSetters() {
+        User user = new User("happyhatter", "underworld", 200);
+        user.setUsername("madhatter");
+        user.setPassword("wonderland");
+        user.setHighScore(300);
+        assertEquals("madhatter", user.getUsername());
+        assertEquals("wonderland", user.getPassword());
+        assertEquals(300, user.getHighScore());
     }
 }
