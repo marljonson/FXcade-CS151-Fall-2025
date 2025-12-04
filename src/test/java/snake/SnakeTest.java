7cf3288b94cca86463f7f8667a123fd4832199b4
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-12-03 17:24:39 -0800

    Initialized SnakeTest class

---
+++ src/test/java/snake/SnakeTest.java
@@ -0,0 +1,17 @@
+package snake;
+
+import java.util.List;
+
+
+import org.junit.jupiter.api.Test;
+
+import snake.model.Direction;
+import snake.model.Snake;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SnakeTest {
+
+    
+
+}
